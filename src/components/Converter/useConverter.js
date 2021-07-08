9b23f7758484a23e942dca52e488ac11d873d9d4
Liam Marcassa <liam@gamesheetinc.com>
2021-07-08 00:22:04 -0400

    Add jerk mitigation to pulse generation algorithm

--- src/components/Converter/useConverter.js
+++ src/components/Converter/useConverter.js
@@ -72,6 +72,24 @@ export default function useConverter() {
         );
       }
 
+      // jerk mitigation: 7 = fastest, 0 = slowest
+      const getSpeed = ( a, b ) => {
+        const dist = eD(a,b);
+        if (dist >= 20) {
+          return 7;
+        } else if (dist >= 10) {
+          return 5;
+        } else if (dist >= 5) {
+          return 3;
+        } else if (dist >= 3) {
+          return 2;
+        } else if (dist >= 1) {
+          return 1;
+        } else {
+          return 0;
+        }
+      }
+
       const pathToCoors = (path) => {
         const out = [];
         const total = unitToMm(path.getTotalLength());
@@ -79,16 +97,18 @@ export default function useConverter() {
         // use 2mm output resolution
         const resolution = 2;
 
-        out.push(
-          pointToCoor(
-            path.getPointAtLength(0)));
+        out.push({
+          speed: 0,
+          coor: pointToCoor(path.getPointAtLength(0))
+        });
 
         if (total > resolution) {
 
-          out.push(
-            pointToCoor(
-              path.getPointAtLength(
-                mmToUnit(resolution))));
+          const resolutionCoor = pointToCoor(path.getPointAtLength(mmToUnit(resolution)));
+          out.push({
+            speed: getSpeed( out[0].coor, resolutionCoor ),
+            coor: resolutionCoor
+          });
 
           if (total > resolution * 2) {
             let lag = 0;
@@ -100,11 +120,11 @@ export default function useConverter() {
               const dist = distToLine(out[lag], out[lag+1], newCoor);
 
               if (dist < precision) {
-                out[lag+1] = newCoor;
+                out[lag+1] = { speed: getSpeed( out[lag].coor, newCoor ), coor: newCoor };
                 precision = precision / 2;
               } else {
                 lag += 1;
-                out.push(newCoor);
+                out.push({ speed: getSpeed( out[lag].coor, newCoor), coor: newCoor });
                 precision = resolution;
               }
               
@@ -113,19 +133,60 @@ export default function useConverter() {
           }
         } 
 
-        out.push(pointToCoor(path.getPointAtLength(path.getTotalLength())));
+        const finalCoor = pointToCoor(path.getPointAtLength(path.getTotalLength()));
+        out.push({ speed: getSpeed( out[out.length - 1].coor, finalCoor), coor: finalCoor });
 
         return out;
       }
 
       // ---- Define functions to convert straight lines to motor moves ----
-      const getPulses = ({ actualLeftSteps, leftDirection, actualRightSteps, rightDirection }) => {
+      const getPulses = ({ actualLeftSteps, leftDirection, actualRightSteps, rightDirection, speeds }) => {
         const mostSteps = Math.max(actualLeftSteps, actualRightSteps);
 
         const leftModulus = actualLeftSteps === 0 ? 0 : mostSteps / actualLeftSteps;
         const rightModulus = actualRightSteps === 0 ? 0 : mostSteps / actualRightSteps;
 
-        const out = new Array(mostSteps).fill(null).map(() => ({ l: 0, r: 0 }));
+        const out = new Array(mostSteps).fill(null).map(() => ({ s: 0, l: 0, r: 0 }));
+
+        if (speeds.current === speeds.top && speeds.top === speeds.eventual) {
+
+          // same speed all the way through
+          out.forEach(o => o.s = speeds.top);
+
+        } else if (speeds.current === speeds.top || speeds.top === speeds.eventual) {
+
+          // slowly ramp from speeds.current to speeds.eventual
+          const speedDiff = speeds.eventual - speeds.current;
+          const sign = speedDiff / Math.abs(speedDiff);
+          const blockSize = out.length / Math.abs(speedDiff);  
+
+          for (let i = 0; i < out.length; i++) {
+            out[i].s = (speeds.current + ( sign * Math.round(i / blockSize) ));
+          }
+
+        } else {
+
+          // ramp from speeds.current to speeds.top
+          // then from speeds.top to speeds.eventual
+          const halfway = Math.round(out.length / 2);
+
+          const firstBaseSpeed = speeds.current;
+          const firstSpeedDiff = speeds.top - firstBaseSpeed;
+          const firstSign = firstSpeedDiff / Math.abs(firstSpeedDiff);
+          const firstBlockSize = halfway / Math.abs(firstSpeedDiff);
+          for (let i = 0; i < halfway; i++) {
+            out[i].s = (firstBaseSpeed + ( firstSign * Math.round(i / firstBlockSize) ));
+          }
+
+          const secondBaseSpeed = (halfway > 0) ? out[halfway-1].s : speeds.current;
+          const secondSpeedDiff = speeds.eventual - secondBaseSpeed;
+          const secondSign = secondSpeedDiff / Math.abs(secondSpeedDiff);
+          const secondBlockSize = (out.length - halfway) / Math.abs(secondSpeedDiff);
+          for (let i = halfway; i < out.length; i++) {
+            out[i].s = (secondBaseSpeed + ( secondSign * Math.round(i / secondBlockSize) ));
+          }
+
+        }
 
         if (leftModulus > 0 && rightModulus > 0) {
           let leftSum = 0;
@@ -167,11 +228,11 @@ export default function useConverter() {
       const mmPerStep = cmToMm(2 * Math.PI * specs['spool-radius'] * specs['step-resolution'] / 360);
       const stepsPerMm = 1 / mmPerStep;
 
-      const traverseStraight = (current, desired) => {
-        const currentLeftCoor = { x: current.x - cmToMm(specs['tool-offset-x']), y: current.y - cmToMm(specs['tool-offset-y'])};
-        const desiredLeftCoor = { x: desired.x - cmToMm(specs['tool-offset-x']), y: desired.y - cmToMm(specs['tool-offset-y'])};
-        const currentRightCoor = { x: current.x + cmToMm(specs['tool-offset-x']), y: current.y - cmToMm(specs['tool-offset-y'])};
-        const desiredRightCoor = { x: desired.x + cmToMm(specs['tool-offset-x']), y: desired.y - cmToMm(specs['tool-offset-y'])};
+      const traverseStraight = (current, desired, nextSpeed) => {
+        const currentLeftCoor = { x: current.coors.x - cmToMm(specs['tool-offset-x']), y: current.coors.y - cmToMm(specs['tool-offset-y'])};
+        const desiredLeftCoor = { x: desired.coors.x - cmToMm(specs['tool-offset-x']), y: desired.coors.y - cmToMm(specs['tool-offset-y'])};
+        const currentRightCoor = { x: current.coors.x + cmToMm(specs['tool-offset-x']), y: current.coors.y - cmToMm(specs['tool-offset-y'])};
+        const desiredRightCoor = { x: desired.coors.x + cmToMm(specs['tool-offset-x']), y: desired.coors.y - cmToMm(specs['tool-offset-y'])};
 
         const leftDelta = eD({ x: 0, y: 0 }, desiredLeftCoor) - eD({ x: 0, y: 0 }, currentLeftCoor);
         const rightDelta = eD({ x: cmToMm(specs['eye-to-eye']), y: 0 }, desiredRightCoor) - eD({ x: cmToMm(specs['eye-to-eye']), y: 0 }, currentRightCoor);
@@ -188,21 +249,39 @@ export default function useConverter() {
         const newLeftLength = eD({ x: 0, y: 0 }, currentLeftCoor) + (actualLeftSteps * mmPerStep * leftDirection);
         const newRightLength = eD({ x: cmToMm(specs['eye-to-eye']), y: 0 }, currentRightCoor) + (actualRightSteps * mmPerStep * rightDirection);
 
-        return { 
-          pulses: getPulses({ actualLeftSteps, leftDirection, actualRightSteps, rightDirection }),
-          newCurrent: solveCoors({ 
+        const pulses = getPulses({
+          actualLeftSteps,
+          leftDirection,
+          actualRightSteps,
+          rightDirection,
+          speeds: {
+            current: current.speed,
+            top: desired.speed,
+            eventual: Math.min(desired.speed, nextSpeed)
+          }});
+
+        const newCurrent = {
+          speed: (!!pulses && pulses.length > 0)
+            ? pulses[pulses.length - 1].s
+            : current.speed,
+          coors: solveCoors({ 
             eyeToEye: cmToMm(specs['eye-to-eye']),
             toolOffsetX: cmToMm(specs['tool-offset-x']),
             toolOffsetY: cmToMm(specs['tool-offset-y']),
             leftLength: newLeftLength,
             rightLength: newRightLength
           })
+        }
+
+        return { 
+          pulses,
+          newCurrent
         };
       }
 
       // ---- Gather all real-world coors we want to hit ----
       const masterCoors = {};
-      masterCoors['start'] = { x: cmToMm(realCoors.outputRect.x), y: cmToMm(realCoors.outputRect.y) };
+      masterCoors['start'] = ({ speed: 0, coor: { x: cmToMm(realCoors.outputRect.x), y: cmToMm(realCoors.outputRect.y) }});
 
       let t1;
       for (let i = 0; i < myPaths.length; i++) {
@@ -215,6 +294,17 @@ export default function useConverter() {
         await updateProgress(`path ${i} converted to coors (${t2-t1} ms)`);
       }
 
+      // ---- Set missing speeds ----
+      for (let i = 0; i < myPaths.length; i++) {
+        const start = (i === 0)
+          ? masterCoors.start.coor
+          : masterCoors[`path-${i-1}`][masterCoors[`path-${i-1}`].length - 1].coor;
+        
+        const end = masterCoors[`path-${i}`][0];
+
+        masterCoors[`path-${i}`][0].speed = getSpeed( start, end );
+      }
+
       // ---- Traverse all paths sequentially ----
       let bigPulseList = [];
 
@@ -222,10 +312,16 @@ export default function useConverter() {
       for (let i = 0; i < myPaths.length; i++) {
         t1 = Date.now();
       
-        for (let coor of masterCoors[`path-${i}`]) {
-          const { newCurrent, pulses } = traverseStraight(current, coor);
+        for (let c = 0; c < masterCoors[`path-${i}`].length; c++) {
+          const nextSpeed = (c+1 >= masterCoors[`path-${i}`].length)
+            ? (i+1 >= myPaths.length)
+              ? 0
+              : masterCoors[`path-${i+1}`][0].speed
+            : masterCoors[`path-${i}`][c+1].speed;
+          const coor = masterCoors[`path-${i}`][c];
+          const { newCurrent, pulses } = traverseStraight(current, coor, nextSpeed);
 
-          if (!newCurrent) {
+          if (!newCurrent.coors) {
             return null;
           }
 
